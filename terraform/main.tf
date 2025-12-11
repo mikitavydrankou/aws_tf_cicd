0f9373b072d722f6dbfdf02e4bc0cfa417827fe3
mikitavydrankou <mikitavydrankou@gmail.com>
2025-12-11 18:23:37 +0100

    ubuntu ami fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,8 +14,18 @@ provider "aws" {
     region = var.region
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] 
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
 resource "aws_instance" "server" {
-    ami = "ami-002e81a9a522f1f19"
+    ami = data.aws_ami.ubuntu.id
     instance_type = "t2.micro"
     key_name = aws_key_pair.deployer.key_name
     vpc_security_group_ids = [aws_security_group.maingroup.id]
